0d0845879171b4b29139ff9ee58d595f22133e14
Nilanjan Majumdar <nilanjanmajumdar@pop-os.localdomain>
2020-09-26 11:16:02 +0530

    added upload feature

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 const fs = require('fs');
+const cors = require('cors')
+const multer = require('multer')
+const upload = multer({ dest: 'assets/' })
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// any request coming in, transfer all body into JSON
+app.use(express.json())
+
+// allow cross origin from client localhost
+app.use(cors())
+
+// creating POST endpoint /file
+app.post('/file', upload.single('file'), (req, res) => {
+  console.log('body', req.file.length, req.file)
+
+  // here you can do anything that you want for the file
+  // ex: you want to save it to database here
+
+  res.json({ success: true })
+})
+
 app.get('/video/:something/:filename', function(req, res) {
     if(req.params.something == "1232"){
     
-        
+    function videostream(){    
+    console.log("Hello")  
     const path = `assets/${req.params.filename}`
     const stat = fs.statSync(path)
     const fileSize = stat.size
@@ -39,6 +60,8 @@ app.get('/video/:something/:filename', function(req, res) {
       res.writeHead(200, head)
       fs.createReadStream(path).pipe(res)
     }
+  }
+  videostream();
 }else{
     res.send("Not allowed!");
 }
